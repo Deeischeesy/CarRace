3fff1384ade5dbe4cfd1e79dbf3155167c7593a5
Danielle <danielleryan2134@gmail.com>
2017-11-23 22:45:24 +0000

    GUI Change

--- src/CarRaceGUI.java
+++ src/CarRaceGUI.java
@@ -1,12 +1,10 @@
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
+import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class CarRaceGUI {
+public class CarRaceGUI extends JPanel{
 
     private JFrame frame;
 
@@ -42,40 +40,44 @@ public class CarRaceGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
         frame.setVisible(true);
+        //frame.add(this);
+
+        //JLabel image1 = new JLabel(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
+
+        //frame.add(image1);
+
 
         JButton btnNewButton = new JButton("New button");
         btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
-        btnNewButton.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
-        btnNewButton.setBounds(10, 11, 76, 34);
-        frame.getContentPane().add(btnNewButton);
+                                           public void actionPerformed(ActionEvent e) {
+                                           }
+                                       });
 
-        JButton btnNewButton_1 = new JButton("bt1");
-        btnNewButton_1.setBounds(0, 236, 89, 23);
-        frame.getContentPane().add(btnNewButton_1);
-        ImageIcon car = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
 
-        JButton btnNewButton_2 = new JButton("bt2");
-        btnNewButton_2.setBounds(0, 183, 89, 23);
-        frame.getContentPane().add(btnNewButton_2);
-        ImageIcon car2 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
 
-        JButton btnNewButton_3 = new JButton("bt3");
-        btnNewButton_3.setBounds(0, 132, 89, 23);
-        frame.getContentPane().add(btnNewButton_3);
-        ImageIcon car3 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+            btnNewButton.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
+            //btnNewButton.setBounds(10, 11, 76, 34);
+		frame.getContentPane().add(btnNewButton);
 
-        JButton btnNewButton_4 = new JButton("bt4");
-        btnNewButton_4.setBounds(0, 76, 89, 23);
-        frame.getContentPane().add(btnNewButton_4);
-        ImageIcon car4 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+            JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(0, 236, 89, 23);
+        btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
+		frame.getContentPane().add(btnNewButton_1);
 
-        JButton btnNewButton_5 = new JButton("bt5");
-        btnNewButton_5.setBounds(0, 26, 89, 23);
-        frame.getContentPane().add(btnNewButton_5);
-        ImageIcon car5 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+            JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(0, 183, 89, 23);
+		frame.getContentPane().add(btnNewButton_2);
+
+            JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(0, 132, 89, 23);
+		frame.getContentPane().add(btnNewButton_3);
+
+            JButton btnNewButton_4 = new JButton("New button");
+		btnNewButton_4.setBounds(0, 76, 89, 23);
+		frame.getContentPane().add(btnNewButton_4);
+
+            JButton btnNewButton_5 = new JButton("New button");
+		btnNewButton_5.setBounds(0, 26, 89, 23);
+		frame.getContentPane().add(btnNewButton_5);
+        }
     }
-}
-
