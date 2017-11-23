c29e13b6e878cd910ebe6b66a26cc95c4f44aa9e
Danielle <danielleryan2134@gmail.com>
2017-11-23 14:40:28 +0000

    updated GUI

--- src/CarRaceGUI.java
+++ src/CarRaceGUI.java
@@ -1,70 +1,81 @@
-import javax.swing.*;
-import java.awt.image.BufferedImage;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class CarRaceGUI extends JFrame{
+public class CarRaceGUI {
 
-    public static int frameWidth;
-    public static int frameHeight;
-    public static final long secInNanosec = 1000000000L;
-    public static final long milisecInNanosec = 1000000L;
-    private final int GAME_FPS = 60;
-    private final long GAME_UPDATE_PERIOD = secInNanosec / GAME_FPS;
-    public static enum GameState{STARTING, VISUALIZING, GAME_CONTENT_LOADING, MAIN_MENU, OPTIONS, PLAYING, GAMEOVER, DESTROYED}
-    public static GameState gameState;
-    private long gameTime;
-    // It is used for calculating elapsed time.
-    private long lastTime;
+    private JFrame frame;
 
-    // The actual game
-    //private Game game;
-    private BufferedImage BettingCarMenuImg;
-
-    public CarRaceGUI(){
-
-     super();
-
-    gameState = GameState.VISUALIZING;
-
-    //We start game in new thread.
-    Thread gameThread = new Thread() {
-        @Override
-        public void run(){
-            GameLoop();
-        }
-
-        private void GameLoop() {
-        }
-    };
-        gameThread.start();
-
-
-
-         //sets the title for the frame
-        this.setTitle("Racing Bets");
-        //setting the size for the frame
-        if(true){
-            //disabling decorations for the frame
-            this.setUndecorated(true);
-            //set the frame to full screen
-            this.setExtendedState(this.MAXIMIZED_BOTH);
-        }
-        else{
-            //size of the frame
-            this.setSize(800,600);
-            //put frame in the center of the screen
-            this.setLocationRelativeTo(null);
-            this.setResizable(false);
-        }
-
-        //exit the application when the user presses the exit button
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        this.setVisible(true);
-
-
-
-
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    CarRaceGUI window = new CarRaceGUI();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
+    /**
+     * Create the application.
+     */
+    public CarRaceGUI() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 635, 343);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        frame.setVisible(true);
+
+        JButton btnNewButton = new JButton("New button");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        btnNewButton.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
+        btnNewButton.setBounds(10, 11, 76, 34);
+        frame.getContentPane().add(btnNewButton);
+
+        JButton btnNewButton_1 = new JButton("bt1");
+        btnNewButton_1.setBounds(0, 236, 89, 23);
+        frame.getContentPane().add(btnNewButton_1);
+        ImageIcon car = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+
+        JButton btnNewButton_2 = new JButton("bt2");
+        btnNewButton_2.setBounds(0, 183, 89, 23);
+        frame.getContentPane().add(btnNewButton_2);
+        ImageIcon car2 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+
+        JButton btnNewButton_3 = new JButton("bt3");
+        btnNewButton_3.setBounds(0, 132, 89, 23);
+        frame.getContentPane().add(btnNewButton_3);
+        ImageIcon car3 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+
+        JButton btnNewButton_4 = new JButton("bt4");
+        btnNewButton_4.setBounds(0, 76, 89, 23);
+        frame.getContentPane().add(btnNewButton_4);
+        ImageIcon car4 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+
+        JButton btnNewButton_5 = new JButton("bt5");
+        btnNewButton_5.setBounds(0, 26, 89, 23);
+        frame.getContentPane().add(btnNewButton_5);
+        ImageIcon car5 = new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg");
+    }
 }
+
