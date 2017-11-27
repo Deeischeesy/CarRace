5a857274f78280e95c44506a6a4307ece96d20f8
Danielle <danielleryan2134@gmail.com>
2017-11-27 12:32:24 +0000

    Working on GUI

--- src/CarBetApp.java
+++ src/CarBetApp.java
@@ -6,7 +6,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.ArrayList;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class CarBetApp extends Bettors {
 
@@ -16,59 +16,53 @@ public class CarBetApp extends Bettors {
     static SimpleDateFormat sdf = new SimpleDateFormat("HH: mm : ss");
 
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        Car c1 = new Car(10,10.0f,20.0f,10,5,10.0f,Color.YELLOW,20.0f);
-        Car c2 = new Car(10,30.0f,20.0f,10,5,10.0f,Color.RED,30.0f);
+
+        Car c1 = new Car(10, 10.0f, 20.0f, 10, 5, 10.0f, Color.YELLOW, 20.0f);
+        Car c2 = new Car(10, 30.0f, 20.0f, 10, 5, 10.0f, Color.RED, 30.0f);
+
 
         List<Bettors> allBettors = new ArrayList<Bettors>();
-        BettingTransaction bet1 = new BettingTransaction("12345678T",  c1, 15f,  "Lose",  "13:15:23");
-        BettingTransaction bet2 = new BettingTransaction("12345678T",  c2, 5f, "Lose", "13:16:28");
 
-        Bettors b1 = new Bettors("Joe Bloggs","Tralee","12345678T",1500,/*new ArrayList<BettingTransaction>()*/ new ArrayList<BettingTransaction>());
+        BettingTransaction bet1 = new BettingTransaction("12345678T", c1, 15f, "Lose", "13:15:23");
+        BettingTransaction bet2 = new BettingTransaction("12345678T", c2, 5f, "Lose", "13:16:28");
+
+        Bettors b1 = new Bettors("Joe Bloggs", "Tralee", "12345678T", 1500,/*new ArrayList<BettingTransaction>()*/ new ArrayList<BettingTransaction>());
 
         b1.getBets().add(bet1); //adding the bets to Joe's array list of bets
         b1.getBets().add(bet2);
 
         allBettors.add(b1); //adding Joe to the array list of bettors
 
-        Bettors b2 = new Bettors("Jane Doe","Dingle","98765432K",2000,/*new ArrayList<BettingTransaction>()*/ new ArrayList<BettingTransaction>());
+        Bettors b2 = new Bettors("Jane Doe", "Dingle", "98765432K", 2000,/*new ArrayList<BettingTransaction>()*/ new ArrayList<BettingTransaction>());
 
         allBettors.add(b2); //adding Jane to the array list of bettors
 
 
-
-        BettingTransaction bt1 = new BettingTransaction("12345678T", c2,30.0f,"win"," minutes");
+        BettingTransaction bt1 = new BettingTransaction("12345678T", c2, 30.0f, "win", " minutes");
         b1.getBets().add(bt1);
 
-        BettingTransaction bt2 = new BettingTransaction("12345678S",c1,40.0f,"lost","minutes");
+        BettingTransaction bt2 = new BettingTransaction("12345678S", c1, 40.0f, "lost", "minutes");
         b2.getBets().add(bt2);
 
-        for(Bettors bettor : allBettors)
+        for (Bettors bettor : allBettors)
             System.out.println(bettor);
-
-
 
 
         String message = "";
         for (Bettors b : allBettors) {
-            message += b.toString();
+            message += message.toString();
         }
         JOptionPane.showMessageDialog(null, message);
-    }
 
-    public void UpdateGame(long gameTime, Point point) {
-    }
-
-    public void Draw(Graphics2D g2d, Point point) {
-    }
-
-    public void DrawGameOver(Graphics2D g2d, Point point) {
-    }
-
-    public void RestartGame() {
     }
 }
+
+
+
+
+
 
 
 
--- src/CarRaceGUI.java
+++ src/CarRaceGUI.java
@@ -1,4 +1,4 @@
-import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.*;
 import java.awt.event.ActionListener;
@@ -21,8 +21,10 @@ public class CarRaceGUI extends JPanel{
                     e.printStackTrace();
                 }
             }
-        });
-    }
+
+            }
+        );}
+
 
     /**
      * Create the application.
@@ -59,25 +61,41 @@ public class CarRaceGUI extends JPanel{
             //btnNewButton.setBounds(10, 11, 76, 34);
 		frame.getContentPane().add(btnNewButton);
 
-            JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(0, 236, 89, 23);
+        setSize(400,400);
+        setVisible(true);
+
+        setLayout(new BorderLayout());
+
+        JLabel background=new JLabel(new ImageIcon("C:\\Users\\Gary\\Pictures\\backgroundimg.jpg"));
+
+        add(background);
+
+        background.setLayout(new FlowLayout());
+
+
+        JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(0, 236, 89, 42);
         btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
 		frame.getContentPane().add(btnNewButton_1);
 
             JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(0, 183, 89, 23);
+		btnNewButton_2.setBounds(0, 183, 89, 42);
+        btnNewButton_2.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
 		frame.getContentPane().add(btnNewButton_2);
 
             JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(0, 132, 89, 23);
+		btnNewButton_3.setBounds(0, 132, 89, 42);
+        btnNewButton_3.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
 		frame.getContentPane().add(btnNewButton_3);
 
             JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.setBounds(0, 76, 89, 23);
+		btnNewButton_4.setBounds(0, 76, 89, 42);
+        btnNewButton_4.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
 		frame.getContentPane().add(btnNewButton_4);
 
             JButton btnNewButton_5 = new JButton("New button");
-		btnNewButton_5.setBounds(0, 26, 89, 23);
+		btnNewButton_5.setBounds(0, 26, 89, 42);
+        btnNewButton_5.setIcon(new ImageIcon("C:\\Users\\Gary\\Pictures\\images.jpg"));
 		frame.getContentPane().add(btnNewButton_5);
         }
     }
